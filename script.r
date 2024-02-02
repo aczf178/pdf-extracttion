34cb4b49f1a51ed4a076bd53ded90b43d99d87aa
aczf178 <51364655+aczf178@users.noreply.github.com>
2024-02-02 23:36:11 +0000

    fix tabulizer issue

---
+++ script.r
@@ -0,0 +1,11 @@
+library(tabulizer)
+library(dplyr)
+library(tidyr)
+library(tibble)
+library(purrr)
+library(janitor)
+library(rJava)
+
+pdf_data <- extract_tables("Bupa-Ltd-Annual-Report-2022-.pdf")
+
+
